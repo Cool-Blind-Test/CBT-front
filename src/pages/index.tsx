05b5e0e191f4cb8205cf2784745046678c766e7e
RangoDisco <rango1@posteo.net>
2022-01-29 17:29:58 +0100

    added about section on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../sass/pages/Home.module.scss";
 import useTranslation from "next-translate/useTranslation";
+import logo from "../assets/logo.svg";
+import { ChevronDown } from "react-feather";
 
 const Home: NextPage = () => {
   const { t } = useTranslation("home");
-
   return (
     <div className={styles.container}>
       <Head>
@@ -16,26 +18,76 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div
-          style={{
-            height: 500,
-            width: 500,
-            backgroundColor: "red",
-            borderRadius: "50%",
-          }}
-        ></div>
-
-        <div className={styles.buttons}>
-          <Link href="/signup">
-            <a className={styles.inscription}>{t("signUp")}</a>
+        <section className={styles.main}>
+          <Image src={logo} alt="CBT Logo" />
+          <div className={styles.buttons}>
+            <Link href="/signup">
+              <a className={styles.inscription}>{t("signUp")}</a>
+            </Link>
+            <Link href="/signin">
+              <a className={styles.connexion}>{t("signIn")}</a>
+            </Link>
+            <Link href="/play">
+              <a className={styles.anonyme}>{t("anon")}</a>
+            </Link>
+          </div>
+          <Link href="#about">
+            <a className={styles.chevronDownDiv}>
+              <ChevronDown size={50} className={styles.chevronDown} />
+            </a>
           </Link>
-          <Link href="/signin">
-            <a className={styles.connexion}>{t("signIn")}</a>
-          </Link>
-          <Link href="/play">
-            <a className={styles.anonyme}>{t("anon")}</a>
-          </Link>
-        </div>
+        </section>
+        <section className={styles.about} id="about">
+          <h2 className={styles.title}>{t("aboutCBT")}</h2>
+          <div className="aboutText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. <br />
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. <br />
+            But I must explain to you how all this mistaken idea of denouncing
+            pleasure and praising pain was born and I will give you a complete
+            account of the system, and expound the actual teachings of the great
+            explorer of the truth, the master-builder of human happiness. No one
+            rejects, dislikes, or avoids pleasure itself, because it is
+            pleasure, but because those who do not know how to pursue pleasure
+            rationally encounter consequences that are extremely painful. <br />
+            Nor again is there anyone who loves or pursues or desires to obtain
+            pain of itself, because it is pain, but because occasionally
+            circumstances occur in which toil and pain can procure him some
+            great pleasure. <br />
+            To take a trivial example, which of us ever undertakes laborious
+            physical exercise, except to obtain some advantage from it? But who
+            has any right to find fault with a man who chooses to enjoy a
+            pleasure that has no annoying consequences, or one who avoids a pain
+            that produces no resultant pleasure?
+          </div>
+          <h2 className={styles.title}>{t("howToPlay")}</h2>
+          <div className="aboutText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. <br />
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. <br />
+            But I must explain to you how all this mistaken idea of denouncing
+            pleasure and praising pain was born and I will give you a complete
+            account of the system, and expound the actual teachings of the great
+            explorer of the truth, the master-builder of human happiness. No one
+            rejects, dislikes, or avoids pleasure itself, because it is
+            pleasure, but because those who do not know how to pursue pleasure
+            rationally encounter consequences that are extremely painful. <br />
+            Nor again is there anyone who loves or pursues or desires to obtain
+            pain of itself, because it is pain, but because occasionally
+            circumstances occur in which toil and pain can procure him some
+            great pleasure.
+          </div>
+        </section>
       </main>
     </div>
   );
